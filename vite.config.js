d884901cb011151e0c2e950106f2f37eba32c3c1
Alfredo <offsound2020@gmail.com>
2024-03-08 15:22:08 -0400

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cpf-vendors-resources/",
+  base: "/cpf-vendors-resources",
   plugins: [react()],
 })
