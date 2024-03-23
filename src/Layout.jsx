61b0a4713804759186466cfedf408667ba5eca7f
Alfredo <offsound2020@gmail.com>
2024-03-23 01:18:47 -0400

    Update

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,7 +6,9 @@ import Roomvo from "./components/Roomvo";
 import Footer from "./components/Footer";
 import MainLoadScreen from "./components/MainLoadScreen";
 import EmblaCarousel from "./components/EmblaCarousel"
-
+import { Link } from "react-router-dom";
+import {motion} from "framer-motion"
+import "./scss/index.scss"
 
 //
 import Loader from "./components/Loader";
@@ -18,6 +20,12 @@ function Layout() {
     <>
       <Header />
       <Suspense fallback={<Loader />}><MainBanner /></Suspense>
+      <div className="inspire-home-banner">
+        <motion.img src="https://cpffloors.com/wp-content/uploads/2024/03/Group-82.svg" alt="" initial={{opacity:0, scale: 3}} animate={{opacity:1, scale: 1}} transition={{duration:3}}/>
+        <button>
+          <Link target="blank" to="https://cpffloors.com/inspire-black-edition-collection/">Visit the Collection</Link>
+        </button>
+      </div>
       <MainCarousel />
       <Divider />
       <EmblaCarousel />
